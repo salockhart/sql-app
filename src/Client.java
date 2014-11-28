f8527a0c269021927641739894cbad577abca005
mattrask <mattrask34@gmail.com>
2014-11-28 14:55:30 +0000

    added operations and text fields for WHERE

--- src/Client.java
+++ src/Client.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  * Client.java
@@ -20,6 +19,8 @@ public class Client extends JFrame implements ActionListener{
 	private ArrayList<JComboBox> selectB;
 	private ArrayList<JComboBox> fromB;
 	private JComboBox whereB;
+	private ArrayList<JComboBox> operationsB;
+	private ArrayList<JTextField> inputs;
 	private JButton run;
 	private JLabel SELECT;
 	private JLabel FROM;
@@ -31,6 +32,7 @@ public class Client extends JFrame implements ActionListener{
 	private JButton wherePlus;
 	private JButton whereMinus;
 	private SQLConnection connection;
+	private static String[] operands= {"==", "<=", "<", ">=", ">", "LIKE"};
 	
 	public static void main(String[] args) {
 		String username, password;
@@ -87,7 +89,10 @@ public class Client extends JFrame implements ActionListener{
 		whereP.add(WHERE);
 		whereB= createBoxColumns(false);
 		whereP.add(whereB);
-//		whereP.add(/* elements */);
+		operationsB.add(createOperations(false));
+		whereP.add(operationsB.get(0));
+		inputs.add(new JTextField());
+		whereP.add(inputs.get(0));
 		whereP.add(wherePlus);
 		whereP.add(whereMinus);
 		
@@ -117,13 +122,25 @@ public class Client extends JFrame implements ActionListener{
 				wherePlus.setEnabled(true);
 				whereMinus.setEnabled(true);
 				
-				//loop for enabling list of other "boxes"
+				for(int i=0; i<operationsB.size(); i++) {
+					operationsB.get(i).setEnabled(true);
+				}
+				
+				for(int i=0; i<inputs.size(); i++) {
+					inputs.get(i).setEnabled(true);
+				}
 			}
 			else {
 				wherePlus.setEnabled(false);
 				whereMinus.setEnabled(false); 
 				
-				//loop for disabling list of other "boxes"
+				for(int i=0; i<operationsB.size(); i++) {
+					operationsB.get(i).setEnabled(false);
+				}
+				
+				for(int i=0; i<inputs.size(); i++) {
+					inputs.get(i).setEnabled(false);
+				}
 			}
 		}
 		
@@ -157,6 +174,27 @@ public class Client extends JFrame implements ActionListener{
 			}
 		}
 		
+		if(e.getSource()== wherePlus) {
+			boolean on= WHERE.isSelected();
+			
+			whereP.remove(wherePlus);
+			whereP.remove(whereMinus);
+			operationsB.add(createOperations(on));
+			whereP.add(operationsB.get(operationsB.size()-1));
+			inputs.add(createField(on));
+			whereP.add(inputs.get(inputs.size()-1));
+			whereP.add(wherePlus);
+			whereP.add(whereMinus);
+		}
+		
+		if(e.getSource()== whereMinus) {
+			//only check operations because operations and inputs grow/shrink together
+			if(operationsB.size()>1) {
+				fromP.remove(operationsB.remove(operationsB.size()-1));
+				fromP.remove(inputs.remove(inputs.size()-1));
+			}
+		}
+		
 	}
 	
 	public JComboBox createBoxTables(boolean enable) {
@@ -167,9 +205,28 @@ public class Client extends JFrame implements ActionListener{
 	}
 	
 	public JComboBox createBoxColumns(boolean enable) {
-		String[] columns= connection.getColumns();
+//		String[] columns= 
 		JComboBox temp= new JComboBox(columns);
 		temp.setEnabled(enable);
 		return temp;
+	}
+	
+	public JComboBox createOperations(boolean enable) {
+		JComboBox temp= new JComboBox(operands);
+		temp.setEnabled(enable);
+		return temp;
+	}
+	
+	public JTextField createField(boolean enable) {
+		JTextField temp= new JTextField();
+		temp.setEnabled(enable);
+		return temp;
+	}
+	
+	public String[] calcTables() {
+		ArrayList<String> temp= new ArrayList<String>();
+		for(int i=0; i<fromB.size(); i++) {
+			temp.add(fromB.get(i).getSelectedItem())
+		}
 	}
 }
